5c80c534a5e2719f5c9a9dacbe92cf808e4e901c
Simon <simon@MacBook-Pro-4.local>
2024-11-11 10:43:11 -0600

    fixed writing.php hopefully

--- writings.php
+++ writings.php
@@ -11,26 +11,35 @@
     <div id="sidebar">
         <h3 id="sidebar-title"><a href="/">manitobaTime</a></h3>
         <a href="/simonPelletier.html" id="sidebar-button">simonPelletier</a><br>
-				<a href="/maddie.html" id="sidebar-button">maddieMW</a><br>
+        <a href="/maddie.html" id="sidebar-button">maddieMW</a><br>
         <a href="/writings.php" id="sidebar-button">Writings</a><br>
         <a href="/art" id="sidebar-button">Art</a><br>
     </div>
 
     <div id="content">
         <h1>My Writings</h1>
-				<p>A collection of complete and half-baked writings.</p>
+        <p>A collection of complete and half-baked writings.</p>
         <ul style="list-style-type: none">
             <?php
-            $dir = '/Users/simon/newManitobaTime/writings';  // Update this path
-            $files = scandir($dir);
+            // Set the directory path based on the environment
+            if ($_SERVER['SERVER_ADDR'] === '127.0.0.1' || $_SERVER['SERVER_NAME'] === 'localhost') {
+                // Local development path
+                $writings_dir = '/Users/simon/newManitobaTime/writings';
+            } else {
+                // Production server path
+                $writings_dir = '/var/www/html/writings';
+            }
 
-						foreach ($files as $file) {
-							if ($file !== '.' && $file !== '..' && pathinfo($file, PATHINFO_EXTENSION) === 'pdf') {
-								$fileName = pathinfo($file, PATHINFO_FILENAME);  // Get the file name without the .pdf extension
-								echo '<li><a href="writings/' . $file . '" target="_blank">' . $fileName . '</a></li>';
-							}
-						}
-						?>
+            // Scan the directory for PDF files
+            $files = scandir($writings_dir);
+
+            foreach ($files as $file) {
+                if ($file !== '.' && $file !== '..' && pathinfo($file, PATHINFO_EXTENSION) === 'pdf') {
+                    $fileName = pathinfo($file, PATHINFO_FILENAME); // Get the file name without .pdf extension
+										echo '<li><a href="/writings/' . str_replace('+', '%20', urlencode($file)) . '" target="_blank">' . $fileName . '</a></li>';
+                }
+            }
+            ?>
         </ul>
     </div>
 </body>
